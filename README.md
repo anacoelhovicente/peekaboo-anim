a2439ec93ab84365f9b2a95791cff1e350ac17c5
anavicente <anacoelhovicente@gmail.com>
2018-05-30 16:47:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# peekaboo
+
+A JavaScript helper for viewport based CSS animations
+
+## About
+
+Peekaboo is a minimal 2k library for viewport animations.
+
+Peekaboo uses IntersectionObserver to check `data-peekaboo` items and adds a `.peekabooed` class if they are visible. For browsers without support it relies on requestAnimationFrame.
+
+With that, you can do all the animations you want, controlling them with CSS.
+
+## Animations
+
+By default peekaboo doesn't provide any animations, so to animate you have to extend the `.peekabooed` class to animate visible items.
+
+You can delay setting the `.peekabooed` class with the `data-peekaboo-delay` attribute. Or if you prefer you can set delays in the CSS.
+
+## Usage
+
+Once you have downloaded Peekaboo you need to include the JavaScript.
+
+```html
+<script src="peekaboo.min.js"></script>
+<script>
+  var peekaboo = new Peekaboo();
+</script>
+```
+
+And then setup the items to observe with the `data-peekaboo` attribute.
+
+```html
+<div data-peekaboo></div>
+<div data-peekaboo data-peekaboo-delay="500"></div>
+<div data-peekaboo="fadeIn" data-peekaboo-delay="500"></div>
+```
+
+## Options
+
+```js
+var peekaboo = new Peekaboo({
+  root: null, //defaults to viewport
+  threshold: 0.25, //the percentage visible to trigger the finishedClass
+  finishedClass: 'peekabooed'
+});
+```
+
+## Licensing
+
+MIT © 2018 [Ana Vicente](http://anavicente.me)
---
+++ peekaboo.js
@@ -0,0 +1,130 @@
+/**
+ * JavaScript helper for viewport based CSS animations
+ * @version v1.0
+ * @author Ana Vicente
+ * @copyright ©2018 Ana Vicente
+ * Released under the MIT license
+ **/
+
+(function(global) {
+  function Peekaboo(options) {
+    this.peekabooed = [];
+    this.options = {
+      root: null,
+      threshold: 0.25,
+      finishedClass: 'peekabooed',
+    };
+
+    this.options = extend(this.options, options);
+
+    var container = this.options.root ? this.options.root : document;
+    this.items = container.querySelectorAll('[data-peekaboo]');
+
+    if (this.items.length) {
+      this.initPeekaboo();
+    }
+  }
+
+  var PK = Peekaboo.prototype;
+
+  PK.initPeekaboo = function() {
+    var hasIntersectionObserver =
+      'IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype;
+
+    if (hasIntersectionObserver) {
+      this.createObserver();
+    } else {
+      //Fallback to requestanimationframe
+      for (var i = 0; i < this.items.length; i++) {
+        this.requestAnim(this.items[i]);
+      }
+    }
+  };
+
+  PK.createObserver = function() {
+    var options = {
+      root: this.options.root,
+      threshold: this.options.threshold,
+    };
+
+    var observer = new IntersectionObserver(this.observeItem.bind(this), options);
+
+    for (var i = 0; i < this.items.length; i++) {
+      observer.observe(this.items[i]);
+    }
+  };
+
+  PK.observeItem = function(entries, observer) {
+    for (var i = 0; i < entries.length; i++) {
+      var entry = entries[i],
+        item = entry.target;
+
+      if (entry.intersectionRatio > this.options.threshold && !this.isPeekabooed(item)) {
+        this.animItem(item);
+
+        observer.unobserve(item);
+      }
+    }
+  };
+
+  PK.requestAnim = function(item) {
+    if (!this.isPeekabooed(item)) {
+      var bounds = item.getBoundingClientRect(),
+        visibleY = window.innerHeight - bounds.height * this.options.threshold,
+        visibleX = window.innerHeight - bounds.width * this.options.threshold;
+
+      if (bounds.top < visibleY && bounds.left < visibleX && bounds.bottom >= 0 && bounds.right >= 0) {
+        this.animItem(item);
+      }
+
+      window.requestAnimationFrame(this.requestAnim.bind(this, item));
+    }
+  };
+
+  PK.animItem = function(item) {
+    var delay = item.getAttribute('data-peekaboo-delay') ? item.getAttribute('data-peekaboo-delay') : 0;
+    var _this = this;
+
+    this.peekabooed.push(item);
+
+    setTimeout(function() {
+      item.classList.add(_this.options.finishedClass);
+    }, delay);
+  };
+
+  PK.isPeekabooed = function(item) {
+    return this.peekabooed.indexOf(item) >= 0;
+  };
+
+  function extend(defaultOpts, userOpts) {
+    var extended = {};
+    var attr;
+
+    for (attr in defaultOpts) {
+      extended[attr] = defaultOpts[attr];
+    }
+
+    for (attr in userOpts) {
+      extended[attr] = userOpts[attr];
+    }
+
+    return extended;
+  }
+
+  if (typeof define === 'function' && define.amd) {
+    //AMD
+    define(function() {
+      return Peekaboo;
+    });
+  } else if (typeof module !== 'undefined' && module.exports) {
+    //node
+    module.exports = Peekaboo;
+  } else {
+    // browser
+    // use string because of Google closure compiler ADVANCED_MODE
+    /* jslint sub:true */
+    global['Peekaboo'] = Peekaboo;
+  }
+})(this);
